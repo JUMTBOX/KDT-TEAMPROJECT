2cccd766258aafb433f1f7337e2ead745c48d2dc
JUMTBOX <yjey12@naver.com>
2023-02-26 23:19:20 +0900

    feat. 우측 메뉴로고 및 현대차 페이지 모방 제작중

--- js/view.js
+++ js/view.js
@@ -1,16 +1,37 @@
 const section = document.querySelector(".scroller-section");
 const slideSection = section.querySelectorAll("div");
 
-let click = -1;
-section.addEventListener("click", () => {
-  click === slideSection.length ? (click = 0) : (click += 1);
-  slideSection[click].style.opacity = "1";
-  slideSection[click].style.transition = "1s";
-  slideSection[click].style.transform = "translateY(0)";
-});
+let pageNum = 0;
 
-// for (let i = 0; i < slideSection.length; i += 1) {
-//   slideSection[i].style.opacity = "1";
-//   slideSection[i].style.transitionDelay = `${1 + 0.75 * i}s`;
-//   slideSection[i].style.transform = "translateY(0)";
-// }
+function dos() {
+  if (pageNum === 1) {
+    slideSection[pageNum - 1].style.opacity = "1";
+    slideSection[pageNum - 1].style.transform = "translateY(0)";
+    slideSection[pageNum - 1].style.transition = "1s";
+  } else {
+    slideSection[pageNum - 1].style.opacity = "1";
+    slideSection[pageNum - 1].style.transform = "translateY(0)";
+    slideSection[pageNum - 1].style.transition = "1s";
+    slideSection.map((item) => {
+      item.style.opacity = "0";
+      item.style.transform = "translateY(50vh)";
+      item.style.transition = "none";
+    });
+  }
+}
+
+section.addEventListener("wheel", (e) => {
+  e.preventDefault();
+  let wheel = e.wheelDeltaY;
+  wheel > 0 ? console.log("업스크롤") : console.log("다운스크롤");
+
+  wheel > 0
+    ? pageNum < slideSection.length && pageNum >= 0
+      ? (pageNum += 1)
+      : (pageNum = 1)
+    : pageNum <= slideSection.length && pageNum > 1
+    ? (pageNum -= 1)
+    : (pageNum = slideSection.length);
+  console.log(pageNum);
+  dos();
+});
