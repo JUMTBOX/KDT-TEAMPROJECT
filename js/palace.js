9d08d2f4483ace9ee8fe867bc7bd206172cd1329
Karis-Sol <karis.baik1124@gmail.com>
2023-03-05 17:48:50 +0900

    style.글씨변경

--- js/palace.js
+++ js/palace.js
@@ -7,7 +7,7 @@ const slidesLength = slideRight.querySelectorAll('div').length
 
 let activeIndex = 0
 
-slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+slideLeft.style.top = `-${(slidesLength - 1) * 70}vh`
 
 upButton.addEventListener('click', () => changeSlide('up'))
 downButton.addEventListener('click', () => changeSlide('down'))
@@ -27,54 +27,11 @@ const changeSlide = (direction) => {
     }
 
     slideRight.style.transform = `translateY(-${activeIndex * sliderHeight}px)`
-    slideLeft.style.transform = `translateY(${activeIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeIndex * (sliderHeight + 10)}px)`
 }
-// banner
-window.addEventListener("scroll", function() {
-  console.log(window.scrollY);
-  if (window.scrollY > 1474) {
-    const elsalvadorSection = document.querySelector(".ds-banner");
-    elsalvadorSection.classList.add("animate");
-  } else {
-    const elsalvadorSection = document.querySelector(".ds-banner");
-    elsalvadorSection.classList.remove("animate");
-  }
 
-});
 
-//덕수궁 test
-const slideContainer = document.querySelector('.slider-container')
-const slideR = document.querySelector('.right-slide')
-const slideL= document.querySelector('.left-slide')
-const upBtn = document.querySelector('.up-button')
-const downBtn= document.querySelector('.down-button')
-const slidesL = slideR.querySelectorAll('div').length
 
-let activeSIndex =0 
-
-slideLeft.style.top = `-${(slidesL -1) * 100}vh`
-
-upBtn.addEventListener('click', () => changeSlide('up'))
-downBtn.addEventListener('click', () => changeSlide('down'))
-
-const changeSlider = (direction) => {
-  const sliderHeight = sliderContainer.clientHeight
-  if(direction === 'up') {
-    activeSIndex++
-    if(activeSIndex > slidesL -1 )  {
-      activeSIndex = 0
-    }
-  }
-  else if (direction === 'down') {
-    activeSIndex--
-    if(activeSIndex > 0) {
-      activeSIndex = slidesL -1
-    }
-  }
-
-  slideR.style.transform = `translateY(-${activeSIndex * sliderHeight}px)`
-  slideL.style.transform = `translateY(${activeSIndex * sliderHeight}px)`
-}
 
 // 창경궁
 let slideIndex = 1;
